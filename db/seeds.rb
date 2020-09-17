c38ec95f56e18dc069eec4b6212058e76fc6235e
theprivatepark <anneypark@gmail.com>
2020-09-16 23:08:27 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "Anney", last_name: "Park")
+Student.create(first_name: "Amy", last_name: "Sikhammountry")
+Student.create(first_name: "Mark", last_name: "Foley")
